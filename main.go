f8f1df0f4a2c439c00163e70f00855e22accdc87
sbstjn <mail@sbstjn.com>
2016-09-19 20:52:47 +0200

    Add support for viper configuration file

--- main.go
+++ main.go
@@ -5,15 +5,27 @@ import (
 
 	"github.com/sbstjn/hanu"
 	"github.com/sbstjn/hanu-example/cmd"
+	"github.com/spf13/viper"
 )
 
 var (
 	// Version is the bot version
 	Version = "0.0.1"
+	// SlackToken will be set by ENV or config file in init()
+	SlackToken = ""
 )
 
+func init() {
+	viper.SetConfigName(".hanu-example")
+	viper.AddConfigPath("$HOME")
+	viper.AutomaticEnv()
+	viper.ReadInConfig()
+
+	SlackToken = viper.GetString("HANU_EXAMPLE_SLACK_TOKEN")
+}
+
 func main() {
-	bot, err := hanu.New("SLACK_API_TOKEN")
+	bot, err := hanu.New(SlackToken)
 
 	if err != nil {
 		log.Fatal(err)
